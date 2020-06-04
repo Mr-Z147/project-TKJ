db312c04583ab922cf5a8a9d00a7fe3969cfff2e
bang-chil <66408810+bang-chil@users.noreply.github.com>
2020-06-04 23:16:28 +0700

    Create tkj.sh

---
+++ tkj.sh
@@ -0,0 +1,237 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+hari=`date +%A`
+tanggal=`date +%d`
+bulan=`date +%B`
+tahun=`date +%Y`
+waktu=`date +%T`
+zona=`date +%Z`
+clear
+# ✿͜͡ঔৣ✪➵͜͡Kanchil-IDঔৣ͜᷍🌹
+# recode gw tampol
+echo $cy"Sedang Memproses Program Tunggu Beberapa Detik"
+sleep 2
+echo $cy"Program berhasil di Proses Selamat Menggunakan.."
+sleep 3
+clear
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"─────────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"                       MODUL TKJ              "$i"         |"
+echo $i" |"$me"───────────────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$i" Tegar Sabila                            "$i"     |"
+echo $i" |"$pu" FACEBOOK :"$ku" SYAA"$i"                                       |"
+echo $i" |"$pu" THANKS TO :"$cy"~ SMK KOMPUTER INDONESIA         "$i"          |"
+echo $i" |"$cy"            ~ ✿͜͡ঔৣ✪➵͜͡PUB_CYBEERঔৣ͜᷍🌹                        "$i"|"
+echo $i" |"$pu" Contack WhatsApp :"$i" 082125068666"$i"                       |"
+echo $i" |"$pu" Contack Gmail :"$i" salsabilamarisa2004@gmail.com"$i"         |"
+echo $i"["$bi"•"$i"]"$me"─────────────────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $i"INFORMASI WAKTU:"
+echo $cy"-HARI"$ku"["$bi"$hari"$ku"]"
+echo $cy"-TANGGAL"$ku"["$bi"$tanggal"$ku"]"
+echo $cy"-BULAN"$ku"["$bi"$bulan"$ku"]"
+echo $cy"-TAHUN"$ku"["$bi"$tahun"$ku"]"
+echo $cy"-WAKTU"$ku"["$bi"$waktu"$ku"]"
+echo $cy"-ZONA"$ku"["$bi"$zona"$ku"]"
+echo
+echo $i"Silahkan Pilih Modul Yang Tersedia"
+echo
+echo $i"─────────────────────────────────────────────────"
+echo $i"|"$me" 1"$i" |"$ku" PENGERTIAN KOMPUTER DAN JARINGAN KOMPUTER"$i" |"
+echo $i"|"$me" 2"$i" |"$ku" CARA INSTALL MICROSOFT OFFICE 2007       "$i" |"
+echo $i"─────────────────────────────────────────────────"
+echo $i"|"$me" 3"$i" |"$ku" INSTALL BAHAN TERMUX "$i"                     |"
+echo $i"|"$me" 4"$i" |"$ku" KELUAR/EXIT PROGRAM      "$i"                 |"
+echo $i"─────────────────────────────────────────────────"
+echo
+echo
+echo $me"┌==="$ku"["$bi"MODUL TKJ"$ku"]"$me"======"$ku"["$bi""Pilih Nomornya""$ku"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+echo "\0033[33;1m"
+echo "
+PENGERTIAN KOMPUTER DAN JARINGAN KOMPUTER
+Pengertian Komputer
+Komputer adalah alat yang dipakai untuk mengolah data menurut prosedur
+yang telah dirumuskan. Kata computer pada awalnya dipergunakan untuk
+menggambarkan orang yang perkerjaannya melakukan perhitungan aritmetika,
+dengan atau tanpa alat bantu, tetapi arti kata ini kemudian dipindahkan
+kepada mesin itu sendiri.
+Kata computer secara umum pernah dipergunakan untuk mendefiniskan orang
+yang melakukan perhitungan aritmetika, dengan atau tanpa mesin pembantu.
+Menurut Barnhart Concise Dictionary of Etymology, kata tersebut digunakan
+dalam bahasa Inggris pada tahun 1646 sebagai kata untuk "orang yang menghitung"
+kemudian menjelang 1897 juga digunakan sebagai "alat hitung mekanis". Selama
+Perang Dunia II kata tersebut menunjuk kepada para pekerja wanita Amerika
+Serikat dan Inggris yang pekerjaannya menghitung jalan artileri perang dengan mesin hitung.
+Pengertian Jaringan Komputer
+Jaringan komputer (jaringan) adalah jaringan telekomunikasi yang memungkinkan antar
+komputer untuk saling berkomunikasi dengan bertukar data. Tujuan dari jaringan
+komputer adalah agar dapat mencapai tujuannya, setiap bagian dari jaringan
+komputer dapat meminta dan memberikan layanan (service). Pihak yang meminta/menerima
+layanan disebut klien (client) dan yang memberikan/mengirim layanan disebut
+peladen (server). Desain ini disebut dengan sistem client-server, dan digunakan
+pada hampir seluruh aplikasi jaringan komputer.
+Dua buah komputer yang masing-masing memiliki sebuah kartu jaringan, kemudian
+dihubungkan melalui kabelmaupun nirkabel sebagai medium transmisi data, dan
+terdapat perangkat lunak sistem operasi jaringan akan membentuk sebuah jaringan
+komputer yang sederhana. Apabila ingin membuat jaringan komputer yang lebih luas
+lagi jangkauannya, maka diperlukan peralatan tambahan seperti Hub, Bridge, Switch,
+Router, Gateway sebagai peralatan interkoneksinya."
+echo
+echo
+read -p "Apakah Kamu Sudah Membaca nya?(y/n): " un
+if [ $un = y ]
+then
+sh project-TKJ.sh
+fi
+fi
+
+if [ $pil = 2 ]
+then
+clear
+echo "\0033[33;1m"
+echo "
+Cara Install Microsoft Office 2007 
+
+Kurang lengkap rasanya jika anda memiliki komputer /
+ laptop namun belum terinstall Microsoft Office. Microsoft
+ Office merupakan salah satu software yang wajib terinstall
+ di komputer anda, terutama jika anda seorang pelajar / karyawan
+yang hampir setiap waktu bergelut dengan yang
+ namanya tugas, perhitungan, presentasi dan lain
+ sebagainya.
+
+
+Salah satu versi Microsoft Office yang paling populer di
+ zamannya adalah Microsoft Office 2007. Meskipun sudah
+ agak jadul, Microsoft Office 2007 masih menjadi primadona
+ banyak orang karena ukuran installernya yang tidak terlalu
+ besar dan fitur-fiturnya masih terbilang “cukup” memenuhi
+ kebutuhan penggunanya.
+
+
+Mayoritas pengguna Microsoft Office 2007 adalah pengguna
+ lama (tahun 2007-2010) yang memang mereka sudah lama
+ menggunakan software office ini dan sudah nyaman
+ dengan menu dan tampilannya. Hal itulah yang membuat
+ mereka tetap setia dengan Microsoft Office 2007 dan
+ enggan untuk berpindah ke versi yang lebih baru.
+
+
+Cara Install Microsoft Office 2007
+
+
+Sebelum melakukan instalasi Microsoft Office 2007 pastikan
+ anda mempunyai file installernya. Jika anda tidak punya,
+ anda bisa mendownload secara gratis melalui link ini. Jika
+ anda sudah selesai mendownloadnya, langsung saja simak
+ langkah-langkah dibawah ini untuk cara menginstall
+ Microsoft Office 2007:
+
+
+1. Saya anggap anda mendownload file installernya. Silakan
+ ektrak filenya dan klik 2X file setup.
+
+
+2. Masukkan serial number pada kolom yang telah
+ disediakan setelah itu pilih Continue.
+
+
+3. Centang I accept the terms of this agreementkemudian
+ pilih Continue.
+
+
+4. Pilih Install Now untuk memulai proses instalasi.
+
+
+5. Tunggu proses instalasi hingga selesai.
+
+
+6. Microsoft Office 2007 sudah berhasil terinstall di
+ komputer / laptop anda. Terakhir pilih Close.
+
+7. Sekarang anda bisa menggunakan Microsoft Office 2007
+ secara maksimal.
+
+
+Seperti itulah cara menginstall Microsoft Office 2007 untuk
+ pemula, mulai dari mendownload file installer sampai
+ selesai proses instalasinya.
+
+
+Perbedaan mendasar dari Microsoft Office 2007 dengan
+ versi yang lebih baru adalah fitur dan tampilannya, tentu
+ yang terbaru lebih kompleks dan memiliki fitur-fitur yang
+ menarik. Jika anda tertarik untuk mencoba versi yang lebih
+ baru, anda bisa membaca tutorialnya melalui link dibawah
+ ini. Saya sudah membuatkannya secara lengkap beserta
+ bagaimana cara melakukan aktivasinya.
+
+
+Namun jika dirasa Microsoft Office 2007 sudah lebih dari
+ cukup memenuhi kebutuhan anda, tidak ada salahnya untuk tetap
+setiap menggunakan Microsoft Office 2007. Semoga
+ tutorial diatas mudah dipahami dan jika anda mendapatkan
+ kendala ketika melakukan instalasi Microsoft Office 2007
+ jangan sungkan untuk menanyakannya melalui kolom
+ komentar dibawah."
+echo
+echo
+read -p "Apakah Kamu Sudah Membaca nya?(y/n): " un
+if [ $un = y ]
+then
+sh project-TKJ.sh
+fi
+fi
+
+if [ $pil = 3 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Telah Menggunakan Program saya"
+sleep 2
+echo $i"Jika Ada Yang Kurang Paham Bisa Tanyakan Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 082126068665"
+echo $ku"Facebook :"$i" SYAA"
+sleep 2
+echo $pur">> Bantu Support Ya Minta Kritik Dan Sarannya <<"
+exit
+fi
+
+
